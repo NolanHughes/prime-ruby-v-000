4d2a9279732896c3531da696c6256af029c77272
Nolan Hughes <nhughes987@gmail.com>
2017-10-27 21:08:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,13 @@
 #   if num <= 1
 #     return false
 #   end
-# 
+#
 #   Math.sqrt(num).to_i.downto(2).each do |i|
 #     if num % i == 0
 #       return false
 #     end
 #   end
-#   
+#
 #   true
 # end
 
